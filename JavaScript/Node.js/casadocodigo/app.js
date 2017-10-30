1c5cbe1144939d0aafd639c2745feae8bde490b5
Cygni94 <ccoelho.me@gmail.com>
2017-10-30 19:39:42 -0200

    29102017

---
+++ JavaScript/Node.js/casadocodigo/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/produtos', function (req, res) {
+    res.render("produtos/lista");
+  });
+
+app.listen(3000, function () {
+  console.log('servidor rodando.');
+});
