eb57c51aaf7342eb7fec80de2df20fcbfb7253ab
Cristiano Coelho <ccoelho.me@gmail.com>
2017-11-09 13:35:10 -0200

    automatizando a adição das listas no index

---
+++ JavaScript/Básico/w3resource/showLists.js
@@ -0,0 +1,22 @@
+/* algoritmo: 
+    addRow=cria row
+    vários appendChild em cascata
+*/
+
+function addRow(listTitle) {
+    let list = document.querySelector("#list");
+    newRow = list.appendChild('div').classList.add("row");
+
+
+    return newRow
+}
+
+console.log(addList);
+console.log(addList());
+
+function newTr(newTd) {
+
+    return newTr
+}
+
+function newTd()
--- JavaScript/Básico/w3resource/showResolutions.js
+++ JavaScript/Básico/w3resource/showResolutions.js
@@ -1,14 +1,3 @@
-function montaTr(two) {
-    var newTr = document.createElement("tr");
-newTr.classList.add("row");
-
-    newTr.appendChild(newTd(two.id)).classList.add("col-1");
-    newTr.appendChild(newTd(two.task)).classList.add("col-3");
-    newTr.appendChild(newTd(two.code)).classList.add("col-6");
-    newTr.appendChild(newTd(two.result)).classList.add("col-2");
-    return newTr;
-}
-
 function newTd(dado) {
     var newTd = document.createElement("td");
     newTd.textContent = dado;
@@ -16,8 +5,19 @@ function newTd(dado) {
     return newTd;
 }
 
+function newTr(task) {
+    var newTr = document.createElement("tr");
+    newTr.classList.add("row");
+
+    newTr.appendChild(newTd(task.id)).classList.add("col-1");
+    newTr.appendChild(newTd(task.task)).classList.add("col-3");
+    newTr.appendChild(newTd(task.code)).classList.add("col-6");
+    newTr.appendChild(newTd(task.result)).classList.add("col-2");
+    return newTr;
+}
+
 function appendRow(task) {
-    let newTr = montaTr(task);
+    let newRow = newTr(task);
     let table = document.querySelector('#table');
-    table.appendChild(newTr);
+    table.appendChild(newRow);
 }
