335dabbf2337272069dfd969f190a0cb9ddc5e8d
Cygni94 <ccoelho.me@gmail.com>
2017-11-09 21:25:10 -0200

    CURSO FINALIZADO

--- JavaScript/React/cdc-admin/src/Autor.js
+++ JavaScript/React/cdc-admin/src/Autor.js
@@ -5,69 +5,60 @@ import PubSub from 'pubsub-js';
 import TratadorErros from  './TratadorErros';
 
 class FormularioAutor extends Component {
-
-  constructor() {
-    super();    
-    this.state = {nome:'',email:'',senha:''};
-    this.enviaForm = this.enviaForm.bind(this);
-    this.setNome = this.setNome.bind(this);
-    this.setEmail = this.setEmail.bind(this);
-    this.setSenha = this.setSenha.bind(this);
-  }
-
-  enviaForm(evento){
-    evento.preventDefault();    
-    $.ajax({
-      url:'http://localhost:8080/api/autores',
-      contentType:'application/json',
-      dataType:'json',
-      type:'post',
-      data: JSON.stringify({nome:this.state.nome,email:this.state.email,senha:this.state.senha}),
-      success: function(novaListagem){
-        PubSub.publish('atualiza-lista-autores',novaListagem);        
-        this.setState({nome:'',email:'',senha:''});
-      }.bind(this),
-      error: function(resposta){
-        if(resposta.status === 400) {
-          new TratadorErros().publicaErros(resposta.responseJSON);
-        }
-      },
-      beforeSend: function(){
-        PubSub.publish("limpa-erros",{});
-      }      
-    });
-  }
-
-  setNome(evento){
-    this.setState({nome:evento.target.value});
-  }
-
-  setEmail(evento){
-    this.setState({email:evento.target.value});
-  }  
-
-  setSenha(evento){
-    this.setState({senha:evento.target.value});
-  }  
-
-    render() {
-        return (
-            <div className="pure-form pure-form-aligned">
-              <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
-                <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome"/>                                              
-                <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email"/>                                              
-                <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha"/>                                                                      
-                <div className="pure-control-group">                                  
-                  <label></label> 
-                  <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
-                </div>
-              </form>             
-
-            </div>  
-
-        );
+  
+    constructor() {
+      super();    
+      this.state = {nome:'',email:'',senha:''};
+      this.enviaForm = this.enviaForm.bind(this);
     }
-}
+  
+    enviaForm(evento){
+      evento.preventDefault();    
+      $.ajax({
+        url:'http://localhost:8080/api/autores',
+        contentType:'application/json',
+        dataType:'json',
+        type:'post',
+        data: JSON.stringify({nome:this.state.nome,email:this.state.email,senha:this.state.senha}),
+        success: function(novaListagem){
+          PubSub.publish('atualiza-lista-autores',novaListagem);        
+          this.setState({nome:'',email:'',senha:''});
+        }.bind(this),
+        error: function(resposta){
+          if(resposta.status === 400) {
+            new TratadorErros().publicaErros(resposta.responseJSON);
+          }
+        },
+        beforeSend: function(){
+          PubSub.publish("limpa-erros",{});
+        }      
+      });
+    }
+  
+    salvaAlteracao(nomeInput,evento){
+      var campoSendoAlterado = {};
+      campoSendoAlterado[nomeInput] = evento.target.value;    
+      this.setState(campoSendoAlterado);   
+    }
+  
+      render() {
+          return (
+              <div className="pure-form pure-form-aligned">
+                <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+                  <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.salvaAlteracao.bind(this,'nome')} label="Nome"/>                                              
+                  <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.salvaAlteracao.bind(this,'email')} label="Email"/>                                              
+                  <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.salvaAlteracao.bind(this,'senha')} label="Senha"/>                                                                      
+                  <div className="pure-control-group">                                  
+                    <label></label> 
+                    <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
+                  </div>
+                </form>             
+  
+              </div>  
+  
+          );
+      }
+  }
 
 class TabelaAutores extends Component {
 
--- JavaScript/React/cdc-admin/src/index.js
+++ JavaScript/React/cdc-admin/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import AutorAdmin from './Autor';
-import LivroAdmin from './Livro';
+import AutorBox from './Autor';
+import LivroBox from './Livro';
 import Home from './Home';
 import './index.css';
 import {BrowserRouter as Router, Route,Switch,Link} from 'react-router-dom';
@@ -12,8 +12,8 @@ ReactDOM.render((
             <App>
                     <Switch>            
                         <Route exact path="/" component={Home}/>
-                        <Route path="/autor" component={AutorAdmin}/>
-                        <Route path="/livro" component={LivroAdmin}/>                
+                        <Route path="/autor" component={AutorBox}/>
+                        <Route path="/livro" component={LivroBox}/>
                     </Switch>            
             </App>
         </Router>
