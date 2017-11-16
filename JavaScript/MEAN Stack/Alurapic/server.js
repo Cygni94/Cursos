13276bd73d5cb34287921f86a78863370b046e9a
Cygni94 <ccoelho.me@gmail.com>
2017-11-16 21:37:29 -0200

    aula 03 OK

---
+++ JavaScript/MEAN Stack/Alurapic/server.js
@@ -0,0 +1,9 @@
+//alurapic/server.js
+
+var http = require('http');
+var app = require('./config/express')
+
+http.createServer(app)
+.listen(3000, function() {
+    console.log('Servidor iniciado');
+});
