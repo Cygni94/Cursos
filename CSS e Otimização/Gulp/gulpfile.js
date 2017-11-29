6172509ba4aeb0cc31fa35d4ed15f66e02807ed6
Cygni94 <ccoelho.me@gmail.com>
2017-11-28 22:30:20 -0200

    Aula 03 OK

--- CSS e Otimização/Gulp/gulpfile.js
+++ CSS e Otimização/Gulp/gulpfile.js
@@ -1,21 +1,39 @@
-// gulpfile.js
-const gulp = require('gulp');
-const imagemin = require('gulp-imagemin');
-const clean = require('gulp-clean');
+//gulpfile.js
+var gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    htmlReplace = require('gulp-html-replace'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    cssmin = require('gulp-cssmin');
 
-//npm rum gulp build-img
-gulp.task('build-img', ['copy'], () =>
-    gulp.src('src/img/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/img'))
-);
+gulp.task('default', ['copy'], function () {
+    gulp.start('build-img', 'usemin');
+});
 
 gulp.task('copy', ['clean'], function () {
- return gulp.src('src/**/*')
-    .pipe(gulp.dest('dist')
-)});
+    return gulp.src('src/**/*')
+        .pipe(gulp.dest('dist'));
+});
 
 gulp.task('clean', function () {
     return gulp.src('dist')
         .pipe(clean());
+});
+
+gulp.task('build-img', function () {
+
+    return gulp.src('dist/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('usemin', function () {
+    return gulp.src('dist/**/*.html')
+        .pipe(usemin({
+            js: [uglify],
+            css: [cssmin]
+        }))
+        .pipe(gulp.dest('dist'));
 });
