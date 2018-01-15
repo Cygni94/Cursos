37c3babd83eead78eb918f4b30417b254c9c2250
Cygni94 <ccoelho.me@gmail.com>
2018-01-14 22:05:22 -0200

    aula 03

--- WordPress/Criacao de um tema do zero/wordpress/wp-content/themes/malura/index.php
+++ WordPress/Criacao de um tema do zero/wordpress/wp-content/themes/malura/index.php
@@ -1,35 +1,30 @@
 <?php get_header(); ?>
+
 <main class="home-main">
-    <div class="container">
+	<div class="container">
 
-        <h1>Bem Vindo ao Maluras!</h1>
-        <ul class="imoveis-listagem">
+		<?php 
+			$args = array( 'post_type' => 'imovel' );
+			$loop = new WP_Query( $args );
+			if( $loop->have_posts() ) { ?>
+			<ul class="imoveis-listagem">
+			<?php while( $loop->have_posts() ) {
+					$loop->the_post(); ?>
 
+				<li class="imoveis-listagem-item">
+					<a href="<?= the_permalink(); ?>">
+						<?php the_post_thumbnail(); ?>
 
-            <?php 
-if( have_posts() ) {
-    while( have_posts() ) {
-        the_post(); ?>
+						<h2><?php the_title(); ?></h2>
 
-            <li class="imoveis-listagem-item">
+						<p><?php the_content(); ?></p>
+					</a>
+				</li>
 
-                <?php the_post_thumbnail(); ?>
-
-                <h2>
-                    <?php the_title(); ?>
-                </h2>
-
-                <div>
-                    <?php the_content(); ?>
-                </div>
-            </li>
-
-            <?php 
-                }
-            }
-            ?>
-        </ul>
-    </div>
+			<?php } ?>
+			</ul>
+		<?php	} ?>
+	</div>
 </main>
 
 <?php get_footer(); ?>
