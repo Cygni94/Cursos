6ddf1022f6292961f5e078fc36bb3535bab452d0
Cristiano Coelho <ccoelho.me@gmail.com>
2018-01-16 17:47:13 -0200

    Task 5 em andamento

--- JavaScript/Básico/w3resource/lists/basic.js
+++ JavaScript/Básico/w3resource/lists/basic.js
@@ -2,7 +2,6 @@
 let one = {
     id: "1",
     task: "Write a JavaScript program to display the current day and time in the following format. Sample Output : Today is : Friday. Current time is : 4 PM : 50 : 22",
-    code: dayAndTime.toString(),
     result: dayAndTime()
 }
 
@@ -22,12 +21,10 @@ function dayAndTime() {
     return result;
 }
 
-
 // exercise 2
 let two = {
     id: "2",
     task: "Write a JavaScript program to print the contents of the current window.",
-    code: printWindow.toString(),
     result: window
 }
 
@@ -40,23 +37,22 @@ function printWindow() {
 let three = {
     id: "3",
     task: "Write a JavaScript program to get the current date. Expected Output : mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy ",
-    code: currentDate.toString(),
     result: currentDate()
 }
 
 function currentDate() {
     let rightNow = new Date;
-let dd = rightNow.getDate();
-if (dd.toString().length == 1) {
-    dd = "0"+dd;
-}
-let mm = rightNow.getMonth() + 1;
-if (dd.toString().length == 1) {
-    dd = "0"+dd;
-}
-let yyyy = rightNow.getFullYear();
+    let dd = rightNow.getDate();
+    if (dd.toString().length == 1) {
+        dd = "0" + dd;
+    }
+    let mm = rightNow.getMonth() + 1;
+    if (dd.toString().length == 1) {
+        dd = "0" + dd;
+    }
+    let yyyy = rightNow.getFullYear();
 
-let today = dd + "/" + mm + "/" + yyyy;
+    let today = dd + "/" + mm + "/" + yyyy;
 
     return today;
 }
@@ -65,7 +61,6 @@ let today = dd + "/" + mm + "/" + yyyy;
 let four = {
     id: "4",
     task: "Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7.",
-    code: escalenoArea.toString(),
     result: escalenoArea()
 }
 
@@ -73,16 +68,30 @@ function escalenoArea() {
     let a = 5;
     let b = 6;
     let c = 7;
-    let semiperimetro = (a + b + c)/2;
-    let area = Math.sqrt(semiperimetro*(semiperimetro-a)*(semiperimetro-b)*(semiperimetro-c));
+    let semiperimetro = (a + b + c) / 2;
+    let area = Math.sqrt(semiperimetro * (semiperimetro - a) * (semiperimetro - b) * (semiperimetro - c));
 
     return area;
 }
 
+//exercise 5
+let five = {
+    id: "5",
+    task: "Write a JavaScript program to rotate the string 'w3resource' in right direction by periodically removing one letter from the end of the string and attaching it to the front.",
+    result: rotateString('w3resource')
+}
 
-var side1 = 5; 
-var side2 = 6; 
-var side3 = 7; 
-var perimeter = (side1 + side2 + side3)/2;
-var area =  Math.sqrt(perimeter*((perimeter-side1)*(perimeter-side2)*(perimeter-side3)));
-console.log(area);
+function rotateString(String) {
+    let array = String.split('');
+    let reversed = array.reverse();
+
+    return reversed;
+}
+
+function animateString(rotateString) {
+    let reversed = rotateString(String);
+    for (let i = 0; i < reversed.length; i++) {
+        reversed[i].classList.add(removing);
+    }
+    
+}
