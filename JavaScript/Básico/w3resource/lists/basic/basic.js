f36187d3f5dd83bfeabf3e19230aee563a1f8744
Cristiano Coelho <ccoelho.me@gmail.com>
2018-01-26 16:53:54 -0200

    260120181653

--- JavaScript/Básico/w3resource/lists/basic/basic.js
+++ JavaScript/Básico/w3resource/lists/basic/basic.js
@@ -2,7 +2,7 @@
 let one = {
     id: "1",
     task: "Write a JavaScript program to display the current day and time in the following format. Sample Output : Today is : Friday. Current time is : 4 PM : 50 : 22",
-    code: dayAndTime.toString(),
+    code: '<code>dayAndTime.toString()</code>',
     result: dayAndTime()
 }
 
