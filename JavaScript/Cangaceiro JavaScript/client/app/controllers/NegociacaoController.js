1cdfc77213c93252d751b10ad6d213763cd709d0
Cygni94 <ccoelho.me@gmail.com>
2018-02-18 15:12:55 -0300

    capítulo 4 concluído

--- JavaScript/Cangaceiro JavaScript/client/app/controllers/NegociacaoController.js
+++ JavaScript/Cangaceiro JavaScript/client/app/controllers/NegociacaoController.js
@@ -1,23 +1,38 @@
 class NegociacaoController {
-    constructor() {
-        let $ = document.querySelector.bind(document);
 
+    constructor() {
+
+        let $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes();
     }
 
     adiciona(event) {
+
         event.preventDefault();
 
-        let converter = new DateConverter();
-        let data = converter.paraData(this._inputData.value);
         let negociacao = new Negociacao(
-            data,
+            DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
-        let diaMesAno = converter.paraTexto(negociacao.data);
-        console.log(diaMesAno);
+
+        //inclui a negociação
+        this._negociacoes.adiciona(negociacao);
+
+        //imprime a lista com o novo elemento
+        console.log(this._negociacoes.paraArray());
+
+        //limpa o formulário
+        this._limpaFormulario();
+    }
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0
+        this._inputData.focus();
     }
 }
