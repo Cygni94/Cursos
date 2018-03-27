3fabf099ae71ad6070230d888e0fdf0dc9b846f8
Cygni94 <ccoelho.me@gmail.com>
2018-03-26 21:20:25 -0300

    ok

--- JavaScript/Cangaceiro JavaScript/client/app/controllers/NegociacaoController.js
+++ JavaScript/Cangaceiro JavaScript/client/app/controllers/NegociacaoController.js
@@ -12,27 +12,26 @@ class NegociacaoController {
     adiciona(event) {
 
         event.preventDefault();
-
-        let negociacao = new Negociacao(
-            DateConverter.paraData(this._inputData.value),
-            parseInt(this._inputQuantidade.value),
-            parseFloat(this._inputValor.value)
-        );
-
-        //inclui a negociação
-        this._negociacoes.adiciona(negociacao);
-
-        //imprime a lista com o novo elemento
-        console.log(this._negociacoes.paraArray());
-
-        //limpa o formulário
+        this._negociacoes.adiciona(this._criaNegociacao());
+        console.log(this._negociacoes);
         this._limpaFormulario();
+        
     }
 
     _limpaFormulario() {
+
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0
         this._inputData.focus();
+    }
+
+    _criaNegociacao() {
+
+        return new Negociacao(
+            DateConverter.paraData(this._inputData.value),
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );
     }
 }
--- JavaScript/Cangaceiro JavaScript/client/app/domain/negociacao/Negociacoes.js
+++ JavaScript/Cangaceiro JavaScript/client/app/domain/negociacao/Negociacoes.js
@@ -1,11 +1,17 @@
 class Negociacoes {
+
     constructor() {
+
         this._negociacoes = [];
     }
+
     adiciona(negociacao) {
+
         this._negociacoes.push(negociacao);
     }
+
     paraArray() {
-        return this._negociacoes;
+
+        return [].concat(this._negociacoes);
     }
 }
