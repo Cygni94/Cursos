46068283ef144f6d637a76685427c317d02529b1
Cygni94 <ccoelho.me@gmail.com>
2018-05-14 21:00:28 -0300

    Aula 01 OK

--- JavaScript/React/React Ninja/github-app/src/App.js
+++ JavaScript/React/React Ninja/github-app/src/App.js
@@ -1,21 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+'use strict'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+import React from 'react'
+
+const App = () => (
+  <div className='app'>
+    <div className='search'>
+      <input type='search' placeholder='Digite o nome do usuário no Github'/>
+    </div>
+    <div className='user-info'>
+      <img src='https://avatars0.githubusercontent.com/u/21344357?v=4'/>
+      <h1>
+        <a href='https://github.com/Cygni94'>Cristiano Coelho</a>
+      </h1>
+
+      <ul className='repos-info'>
+        <li>Repositórios: 5</li>
+        <li>Seguidores: 10</li>
+        <li>Seguindo: 10</li>
+      </ul>
+      <div className='actions'>
+        <button>Ver repositórios</button>
+        <button>Ver favoritos</button>
       </div>
-    );
-  }
-}
 
-export default App;
+      <div className='repos'>
+        <h2>Repositórios</h2>
+        <ul className='repos'>
+          <li><a href='#'>Nome do repositório</a></li>
+        </ul>
+      </div>
+
+      <div className='starred'>
+        <h2>Favoritos</h2>
+        <ul className='repos'>
+          <li><a href='#'>Nome do repositório</a></li>
+        </ul>
+      </div>
+
+      </div>
+    </div>
+
+)
+
+export default App
