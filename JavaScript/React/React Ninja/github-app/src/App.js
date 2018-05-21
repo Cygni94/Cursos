37f466ac4025af21e0c06a34ff955459c1fa5b09
Cygni94 <ccoelho.me@gmail.com>
2018-05-21 00:50:07 -0300

    PropTypes instalado

--- JavaScript/React/React Ninja/github-app/src/App.js
+++ JavaScript/React/React Ninja/github-app/src/App.js
@@ -1,45 +1,34 @@
 'use strict'
 
 import React from 'react'
+import Search from './components/search';
+import UserInfo from './components/user-info';
+import Actions from './components/actions';
+import Repos from './components/repos';
 
 const App = () => (
   <div className='app'>
-    <div className='search'>
-      <input type='search' placeholder='Digite o nome do usuário no Github'/>
-    </div>
-    <div className='user-info'>
-      <img src='https://avatars0.githubusercontent.com/u/21344357?v=4'/>
-      <h1 className='username'>
-        <a href='https://github.com/Cygni94'>Cristiano Coelho</a>
-      </h1>
+<Search/>
+<UserInfo/>
+<Actions/>
+<Repos
+  className='repos' 
+  title='Repositórios'
+  repos={[{
+    name: 'Nome do repositório',
+    link: '#'
+  }]}
+/>
+<Repos
+  className='starred' 
+  title='Favoritos'
+  repos={[{
+    name: 'Nome do repositório',
+    link: '#'
+  }]}
+/>
 
-      <ul className='repos-info'>
-        <li>Repositórios: 5</li>
-        <li>Seguidores: 10</li>
-        <li>Seguindo: 10</li>
-      </ul>
-      <div className='actions'>
-        <button>Ver repositórios</button>
-        <button>Ver favoritos</button>
-      </div>
-
-      <div className='repos'>
-        <h2>Repositórios</h2>
-        <ul className='repos'>
-          <li><a href='#'>Nome do repositório</a></li>
-        </ul>
-      </div>
-
-      <div className='starred'>
-        <h2>Favoritos</h2>
-        <ul className='repos'>
-          <li><a href='#'>Nome do repositório</a></li>
-        </ul>
-      </div>
-
-      </div>
-    </div>
-
+</div>
 )
 
 export default App
