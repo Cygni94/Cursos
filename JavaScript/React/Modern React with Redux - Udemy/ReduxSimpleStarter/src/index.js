4bcc6b98ce1d017d16ebb07da1f5088bb0d25596
Cygni94 <ccoelho.me@gmail.com>
2018-06-15 21:27:14 -0300

    prettier installed

--- JavaScript/React/Modern React with Redux - Udemy/ReduxSimpleStarter/src/index.js
+++ JavaScript/React/Modern React with Redux - Udemy/ReduxSimpleStarter/src/index.js
@@ -1,35 +1,34 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
-import SearchBar from './components/search_bar';
-import VideoList from './components/video_list';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
 
-const API_KEY = 'AIzaSyAM-jlaaCPIEunuSrtOwxyLKgxORjC2Akg';
+const API_KEY = "AIzaSyAM-jlaaCPIEunuSrtOwxyLKgxORjC2Akg";
 
 //Create a new component. This component should produce some HTML
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = { videos: [] };
+    this.state = { videos: [] };
 
-        YTSearch({key: API_KEY, term: 'kefera'}, (videos) => {
-            this.setState({ videos });
+    YTSearch({ key: API_KEY, term: "kefera" }, videos => {
+      this.setState({ videos });
     });
+  }
 
-}
-
-    render() {
-        return (
-            <div>
-                <SearchBar />
-                <VideoList videos={this.state.videos} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 //Take this component's generated HTML and put it on the page (the DOM)
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector(".container"));
