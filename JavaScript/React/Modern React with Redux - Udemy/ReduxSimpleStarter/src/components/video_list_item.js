08a34c1ea30af7121174a716dadf7c2cf15b735f
Cygni94 <ccoelho.me@gmail.com>
2018-06-17 00:43:53 -0300

    grande progresso

--- JavaScript/React/Modern React with Redux - Udemy/ReduxSimpleStarter/src/components/video_list_item.js
+++ JavaScript/React/Modern React with Redux - Udemy/ReduxSimpleStarter/src/components/video_list_item.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React from "react";
 
- const VideoListItem = (props) => {
+const VideoListItem = ({ video, onVideoSelect }) => {
+  const imageUrl = video.snippet.thumbnails.default.url;
+  const videoTitle = video.snippet.title;
 
-    return <li>Video</li>
- };
+  return (
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img src={imageUrl} alt={videoTitle} className="media-object" />
+        </div>
+      </div>
+      <div className="media-body">
+        <div className="media-heading">{video.snippet.title}</div>
+      </div>
+    </li>
+  );
+};
 
- export default VideoListItem;
+export default VideoListItem;
--- JavaScript/React/Modern React with Redux - Udemy/ReduxSimpleStarter/src/index.js
+++ JavaScript/React/Modern React with Redux - Udemy/ReduxSimpleStarter/src/index.js
@@ -3,19 +3,23 @@ import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
-
+import VideoDetail from "./components/video_detail";
 const API_KEY = "AIzaSyAM-jlaaCPIEunuSrtOwxyLKgxORjC2Akg";
-
-//Create a new component. This component should produce some HTML
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
 
     YTSearch({ key: API_KEY, term: "kefera" }, videos => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0],
+      });
     });
   }
 
@@ -23,12 +27,14 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
 }
-
-//Take this component's generated HTML and put it on the page (the DOM)
 
 ReactDOM.render(<App />, document.querySelector(".container"));
