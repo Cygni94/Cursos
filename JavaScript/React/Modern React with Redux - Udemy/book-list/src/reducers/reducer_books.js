692bb0ca21655c0570bd3bdfb58a74247014f91a
Cygni94 <ccoelho.me@gmail.com>
2018-06-17 22:29:24 -0300

    oks

---
+++ JavaScript/React/Modern React with Redux - Udemy/book-list/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "Aniquilação" },
+    { title: "O Fim da Infância" },
+    { title: "Pálido Ponto Azul" },
+    { title: "A Nascente" },
+  ];
+}
